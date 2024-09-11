b3219ca745fc49beb43bc3224ee9269134a90d69
Msarazua <sarazuamaria@gmail.com>
2024-09-10 23:23:13 -0600

    Insertar requerimientos.

--- app/Http/Controllers/RequirementsController.php
+++ app/Http/Controllers/RequirementsController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Requirement;
+use App\Models\RequirementDetail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class RequirementsController extends Controller
 {
@@ -13,7 +17,7 @@ class RequirementsController extends Controller
      */
     public function index()
     {
-        //
+        return view('requirement.create');
     }
 
     /**
@@ -34,7 +38,30 @@ class RequirementsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        DB::beginTransaction();
+        try {
+            $requirement = new Requirement;
+            $requirement->user_id = $request->user_id;
+            $requirement->requirement_title = $request->requirement_title;
+            $requirement->area = $request->area;
+            $requirement->priority = $request->priority;
+            $requirement->description = $request->description;
+            $requirement->references = $request->references;
+            $requirement->save();
+            $requirement_detail = new RequirementDetail;
+            $requirement_detail->requirement_id = $requirement->id;
+            if ($request->hasFile('files')) {
+                $requirement_detail->files = $request->file('files')->store('public');
+            }
+            $requirement_detail->save();
+        } catch (\Illuminate\Database\QueryException $e) {
+            DB::rollBack();
+            dd($e);
+            abort(500, $e->errorInfo[2]);
+            return response()->json($response, 500);
+        }
+        DB::commit();
+        return redirect()->action([RequirementsController::class, 'index'])->with(['mensaje' => 'Solicitud enviada con éxito']);
     }
 
     /**
--- resources/views/requirement/create.blade.php
+++ resources/views/requirement/create.blade.php
@@ -5,56 +5,56 @@
     <div class="card">
       <div class="card-body">
         <h4 class="card-title">Crear una nueva solicitud</h4>
-            <form class="forms-sample material-form bordered" method="POST" action="{{ route('login') }}">
-                @csrf
-                <input type="hidden" required="required" name="user_id">
-                <div class="form-group">
-                    <input type="text" required="required" name="requirement_title">
-                    <label for="input" class="control-label">Título de la solicitud</label><i class="bar"></i>
+        <form class="forms-sample material-form bordered" method="POST" action="{{ route('requerimientos.store') }}" enctype="multipart/form-data">
+            @csrf
+            <input type="hidden" value="{{ auth()->user()->id }}" required="required" name="user_id">
+            <div class="form-group">
+                <input type="text" required="required" name="requirement_title">
+                <label for="input" class="control-label">Título de la solicitud</label><i class="bar"></i>
+            </div>
+            <div class="form-group">
+                <select name="area" class="js-example-basic-single w-100">
+                    <option selected>Área de solicitud</option>
+                    <option value="1">Suscripciones</option>
+                    <option value="2">Redacción</option>
+                    <option value="3">B2B</option>
+                </select>
+              </div>
+            <div class="mt-5">
+                <p class="card-description">Prioridad </p>
+                <div class="form-check">
+                   <label class="form-check-label">
+                   <input name="priority" type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="0"> Alta <i class="input-helper"></i></label>
                 </div>
-                <div class="form-group">
-                    <select name="area" class="js-example-basic-single w-100">
-                        <option selected>Área de solicitud</option>
-                        <option value="1">Suscripciones</option>
-                        <option value="2">Redacción</option>
-                        <option value="3">B2B</option>
-                    </select>
-                  </div>
-                <div class="mt-5">
-                    <p class="card-description">Prioridad </p>
-                    <div class="form-check">
-                       <label class="form-check-label">
-                       <input name="priority" type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="0"> Alta <i class="input-helper"></i></label>
-                    </div>
-                    <div class="form-check">
-                       <label class="form-check-label">
-                       <input name="priority" type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios2" value="1" checked=""> Media <i class="input-helper"></i></label>
-                    </div>
-                    <div class="form-check">
-                        <label class="form-check-label">
-                        <input name="priority" type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios2" value="2" checked=""> Baja <i class="input-helper"></i></label>
-                     </div>
+                <div class="form-check">
+                   <label class="form-check-label">
+                   <input name="priority" type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios2" value="1" checked=""> Media <i class="input-helper"></i></label>
                 </div>
-                <div class="form-group">
-                    <div class="form-floating">
-                        <textarea name="description" class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
-                        <label for="floatingTextarea2">Descripción</label><i class="bar"></i>
-                    </div>
+                <div class="form-check">
+                    <label class="form-check-label">
+                    <input name="priority" type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios2" value="2" checked=""> Baja <i class="input-helper"></i></label>
+                 </div>
+            </div>
+            <div class="form-group">
+                <div class="form-floating">
+                    <textarea name="description" class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+                    <label for="floatingTextarea2">Descripción</label><i class="bar"></i>
                 </div>
-                <div class="form-group">
-                    <div class="form-floating">
-                        <textarea name="references" class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
-                        <label for="floatingTextarea2">Referencias</label><i class="bar"></i>
-                    </div>
+            </div>
+            <div class="form-group">
+                <div class="form-floating">
+                    <textarea name="references" class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+                    <label for="floatingTextarea2">Referencias</label><i class="bar"></i>
                 </div>
-                <div class="input-group mb-3">
-                    <input name="files" type="file" class="form-control" id="inputGroupFile02">
-                    <label class="input-group-text" for="inputGroupFile02">Subir archivo</label>
-                </div>
-                <div class="button-container text-center">
-                    <a type="button" class="button btn btn-primary"><span>Enviar solicitud</span></a>
-                </div>
-            </form>
+            </div>
+            <div class="input-group mb-3">
+                <input name="files" type="file" class="form-control" id="inputGroupFile02">
+                <label class="input-group-text" for="inputGroupFile02">Subir archivo</label>
+            </div>
+            <div class="button-container text-center">
+                <button type="submit" class="button btn btn-primary"><span>Enviar solicitud</span></button >
+            </div>
+        </form>
       </div>
     </div>
   </div>
